4621a138848cf47d27430401c9e908e7be8dba24
apples-father <kostya@wert.io>
2022-05-05 16:42:24 +0430

    Test corrected

--- widget.test.js
+++ widget.test.js
@@ -2,64 +2,50 @@
   * @jest-environment jsdom
 */
 
-let widget, widgetLink
+let widget;
+let widgetLink;
+const WertWidget = require('./index.js');
 
 const getScript = (url) => {
   return new Promise((resolve, reject) => {
-
-    const http = require('http'), https = require('https')
-
-    const client = url.startsWith('https') ? https : http
+    const http = require('http');
+    const https = require('https');
+    const client = url.startsWith('https') ? https : http;
 
     client.get(url, (resp) => {
-        let data = ''
+        let data = '';
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => data += chunk)
+        resp.on('data', (chunk) => data += chunk); // A chunk of data has been recieved
+        resp.on('end', () => resolve(data)); // The whole response has been received
 
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => resolve(data))
-
-    }).on("error", (err) => reject(err))
-  })
+    }).on('error', (err) => reject(err));
+  });
 }
 
 beforeEach((done) => {
+  widget = new WertWidget({
+    container_id: 'widget',
+    partner_id: 'default',
+  });
 
-  getScript(`https://javascript.wert.io/wert-${require('./package.json').version}.js`)
-    .then(data => {
+  widgetLink = widget.getEmbedUrl();
 
-      eval(data)
+  done();
+});
 
-      widget = new WertWidget({
-        container_id: 'widget',
-      })
-
-      widgetLink = widget.getEmbedUrl()
-
-    })
-    .finally(done)
-
-})
-
-
-test('load page via link', () => {
-
+test('Load widget via link', () => {
   return expect(getScript(widgetLink))
-    .resolves.not.toBe('')
+    .resolves.not.toBe('');
+});
 
-})
+test('Mount iframe', () => {
+  const startWidget = `<div id="widget">`;
+  const endWidget = `</div>`;
 
-test('mount iframe', () => {
+  document.body.innerHTML = startWidget + endWidget;
 
-  const startWidget = `<div id="widget">`
-  const endWidget = `</div>`
-
-  document.body.innerHTML = startWidget + endWidget
-
-  widget.mount()
+  widget.mount();
 
   expect(document.body.innerHTML)
     .toBe(`${startWidget}<iframe style="width: 100%; height: 100%;" src="${widgetLink}" allow="camera *; microphone *"></iframe>${endWidget}`)
-
-})
+});
