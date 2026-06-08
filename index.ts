82fd390000dcf8f646ebf57ce70015de8a12eef0
Evgenii <ametist16@gmail.com>
2026-06-08 16:28:15 +0400

    added attribute allow-popups-to-escape-sandbox

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ class WertWidget {
     this.iframe.setAttribute('allow', 'camera *; microphone *; payment');
     this.iframe.setAttribute(
       'sandbox',
-      'allow-scripts allow-forms allow-popups allow-same-origin'
+      'allow-scripts allow-forms allow-popups allow-same-origin allow-popups-to-escape-sandbox'
     );
     this.iframe.setAttribute('data-version', version);
 
